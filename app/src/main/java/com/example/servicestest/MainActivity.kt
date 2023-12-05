447dead502557ac868b89e3cac90c95b0dc05c00
OtsokovShA <shamil24@mail.ru>
2023-12-05 23:57:13 +0300

    Добавление модуля MyServices

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.servicestest
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.simpleService.setOnClickListener {
+            startService(MyServices.newIntent(this))
+        }
     }
 }
